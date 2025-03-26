028c066afedc805eb2cdea540a3aecb61eed12d8
FredericoGarciasAlves <garciasalvesfrederico@gmail.com>
2025-03-26 11:46:18 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// const express = require("express");
+
+// const app = express();
+// const PORT = 3000;
+
+// app.get("/api/random-numbers", (req, res) => {
+//     const numbers = Array.from(
+//         { length: 6 },
+//         () => Math.floor(Math.random() * 100) + 1
+//     );
+//     res.json({ random_numbers: numbers });
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Servidor rodando em http://localhost:${PORT}`);
+// });
---
+++ weeklyAgenda.js
@@ -0,0 +1,5 @@
+const $weeklyClock = document.getElementById("weekly-clock");
+const $weeklyHand = document.getElementById("weekly-hand");
+const $weeklyHourDisplay = document.getElementById("weekly-hour-display");
+const $weeklySelectOption = document.getElementById("weekly-select-option");
+const $weeklyClose = document.getElementById("weekly-close");
